e92876041c795b54174b97c006573dfdee153a56
seifboujemaa <seifeddine.boujemaa@esprit.tn>
2013-12-24 01:35:29 +0100

    Signed-off-by: seifboujemaa <seifeddine.boujemaa@esprit.tn>

--- jsfproject/src/jsfproject/bean/AdministratrBean.java
+++ jsfproject/src/jsfproject/bean/AdministratrBean.java
@@ -24,18 +24,18 @@ import tn.edu.esprit.pidev.artofdev.liveup.ejb.services.reporter.ReporterLocal;
 
 @ManagedBean
 @ViewScoped
-public class AdministratrBean implements Serializable{
+public class AdministratrBean implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	@EJB
-	private AdministratorServicesLocal  local ;
+	private AdministratorServicesLocal local;
 	@EJB
 	private AgentServicesLocal localAgent;
 	@EJB
-    private ChefEditorLocal localchef;
+	private ChefEditorLocal localchef;
 	@EJB
 	private ReporterLocal localReporter;
 	@EJB
@@ -45,303 +45,306 @@ public class AdministratrBean implements Serializable{
 	private Agent agent = new Agent();
 	private Reporter reporter = new Reporter();
 	private SubscribedClient client = new SubscribedClient();
-	
-	
-	
+
 	private List<Agent> agents = new ArrayList<Agent>();
 	private List<ChefEditor> chefEditors = new ArrayList<ChefEditor>();
 	private List<Reporter> reporters = new ArrayList<Reporter>();
 	private List<SubscribedClient> clients = new ArrayList<SubscribedClient>();
-	
-	
-	private boolean bool ;
+
+	private boolean bool;
 	private boolean boolDelete;
 	private boolean boolUpdate;
 	private boolean boolAdd;
-	
-	
-	
-	
+
 	public LambdaServicesLocal getLocalClient() {
 		return localClient;
 	}
+
 	public void setLocalClient(LambdaServicesLocal localClient) {
 		this.localClient = localClient;
 	}
+
 	public SubscribedClient getClient() {
 		return client;
 	}
+
 	public void setClient(SubscribedClient client) {
 		this.client = client;
 	}
+
 	public List<SubscribedClient> getClients() {
 		return clients;
 	}
+
 	public void setClients(List<SubscribedClient> clients) {
 		this.clients = clients;
 	}
+
 	public ReporterLocal getLocalReporter() {
 		return localReporter;
 	}
+
 	public void setLocalReporter(ReporterLocal localReporter) {
 		this.localReporter = localReporter;
 	}
+
 	public Reporter getReporter() {
 		return reporter;
 	}
+
 	public void setReporter(Reporter reporter) {
 		this.reporter = reporter;
 	}
+
 	public List<Reporter> getReporters() {
 		return reporters;
 	}
+
 	public void setReporters(List<Reporter> reporters) {
 		this.reporters = reporters;
 	}
+
 	public List<ChefEditor> getChefEditors() {
 		return chefEditors;
 	}
+
 	public void setChefEditors(List<ChefEditor> chefEditors) {
 		this.chefEditors = chefEditors;
 	}
+
 	public ChefEditorLocal getLocalchef() {
 		return localchef;
 	}
+
 	public void setLocalchef(ChefEditorLocal localchef) {
 		this.localchef = localchef;
 	}
+
 	public ChefEditor getChefEditor() {
 		return chefEditor;
 	}
+
 	public void setChefEditor(ChefEditor chefEditor) {
 		this.chefEditor = chefEditor;
 	}
+
 	public boolean isBoolDelete() {
 		return boolDelete;
 	}
+
 	public void setBoolDelete(boolean boolDelete) {
 		this.boolDelete = boolDelete;
 	}
+
 	public boolean isBoolUpdate() {
 		return boolUpdate;
 	}
+
 	public void setBoolUpdate(boolean boolUpdate) {
 		this.boolUpdate = boolUpdate;
 	}
+
 	public boolean isBoolAdd() {
 		return boolAdd;
 	}
+
 	public void setBoolAdd(boolean boolAdd) {
 		this.boolAdd = boolAdd;
 	}
+
 	public boolean isBool() {
 		return bool;
 	}
+
 	public void setBool(boolean bool) {
 		this.bool = bool;
 	}
+
 	public AdministratorServicesLocal getLocal() {
 		return local;
 	}
+
 	public void setLocal(AdministratorServicesLocal local) {
 		this.local = local;
 	}
+
 	public Agent getAgent() {
 		return agent;
 	}
+
 	public void setAgent(Agent agent) {
 		this.agent = agent;
 	}
 
 	public AdministratrBean() {
-		// TODO Auto-generated constructor stub
+
 	}
-	
+
 	public AgentServicesLocal getLocalAgent() {
 		return localAgent;
 	}
+
 	public void setLocalAgent(AgentServicesLocal localAgent) {
 		this.localAgent = localAgent;
 	}
+
 	public List<Agent> getAgents() {
 		return agents;
 	}
+
 	public void setAgents(List<Agent> agents) {
 		this.agents = agents;
 	}
-	
-	
-	
-	
-	public void formDisplayAdd(){
+
+	public void formDisplayAdd() {
 		boolAdd = true;
 		boolDelete = false;
 		boolUpdate = false;
-		bool =true;
-		
+		bool = true;
+
 	}
-	
-	public String formDisplayDelete(){
+
+	public String formDisplayDelete() {
 		boolAdd = false;
 		boolDelete = true;
 		boolUpdate = false;
-		bool =true;
+		bool = true;
 		return null;
 	}
-	
-	public String formDisplayUpdate(){
+
+	public String formDisplayUpdate() {
 		boolAdd = false;
 		boolDelete = false;
 		boolUpdate = true;
-		bool =true;
+		bool = true;
 		return null;
 	}
-	
-	public String formHide(){
+
+	public String formHide() {
 		bool = false;
 		return null;
 	}
-	
-	
-	
-	
-	public  String doAddAgent(){
-		
+
+	public String doAddAgent() {
+
 		local.createAgent(agent);
 		agent = new Agent();
-		agents=localAgent.findAllAgent();
+		agents = localAgent.findAllAgent();
 		bool = false;
 		return null;
 	}
-	
-	
-	
+
 	@PostConstruct
-	public void init()
-	{
-		agents=localAgent.findAllAgent();
+	public void init() {
+		agents = localAgent.findAllAgent();
 		chefEditors = localchef.findAllChefs();
 		reporters = localReporter.findAllReporter();
-		clients  = localClient.findAllClient();
+		clients = localClient.findAllClient();
 	}
-	
-	
 
-	
-	
-	public String doDeleteAgent(){
-		
+	public String doDeleteAgent() {
+
 		localAgent.deleteAgent(agent);
 		agent = new Agent();
-		bool =false;
-		agents=localAgent.findAllAgent();
+		bool = false;
+		agents = localAgent.findAllAgent();
 		return null;
 	}
-	
-	public String doUpdate(){
+
+	public String doUpdate() {
 		localAgent.updateAgent(agent);
 		agent = new Agent();
-		bool=false;
-		agents=localAgent.findAllAgent();
+		bool = false;
+		agents = localAgent.findAllAgent();
 		return null;
-		
+
 	}
-	
-	public String doAddChef(){
-		
+
+	public String doAddChef() {
+
 		localchef.addChefEditor(chefEditor);
 		chefEditor = new ChefEditor();
 		bool = false;
 		chefEditors = localchef.findAllChefs();
 		return null;
-		}
-	
-	
-	public String doDeleteChef(){
-		
+	}
+
+	public String doDeleteChef() {
+
 		localchef.deleteChefEditor(chefEditor);
 		chefEditor = new ChefEditor();
 		bool = false;
 		chefEditors = localchef.findAllChefs();
-		return null ;
+		return null;
 	}
 
+	public String doUpdateChef() {
 
-	public String doUpdateChef(){
-		
 		localchef.updateChefEditor(chefEditor);
 		chefEditor = new ChefEditor();
 		bool = false;
 		chefEditors = localchef.findAllChefs();
-		return null ;
+		return null;
 	}
-	
-	
-	public String doUpdateReporter(){
-		
+
+	public String doUpdateReporter() {
+
 		localReporter.updateReporter(reporter);
 		reporter = new Reporter();
 		bool = false;
 		reporters = localReporter.findAllReporter();
-		return null ;
+		return null;
 	}
-	
-public String doAddReporter(){
-		
+
+	public String doAddReporter() {
+
 		localReporter.addReporter(reporter);
 		reporter = new Reporter();
 		bool = false;
 		reporters = localReporter.findAllReporter();
-		return null ;
+		return null;
 	}
 
-public String doDeleteReporter(){
-	
-	localReporter.deleteReporter(reporter);
-	reporter = new Reporter();
-	bool = false;
-	reporters = localReporter.findAllReporter();
-	return null ;
-}
+	public String doDeleteReporter() {
 
+		localReporter.deleteReporter(reporter);
+		reporter = new Reporter();
+		bool = false;
+		reporters = localReporter.findAllReporter();
+		return null;
+	}
 
+	public String doAddClient() {
 
-public String doAddClient(){
-	
-	localClient.addClient(client);
-	client = new SubscribedClient();
-	bool = false;
-	clients = localClient.findAllClient();
-	return null ;
-}
+		localClient.addClient(client);
+		client = new SubscribedClient();
+		bool = false;
+		clients = localClient.findAllClient();
+		return null;
+	}
 
-public String doDeleteClient(){
-	
-	localClient.deleteClient(client);
-	client = new SubscribedClient();
-	bool = false;
-	clients = localClient.findAllClient();
-	return null ;
-}
+	public String doDeleteClient() {
 
-public String doUpdateClient(){
-	
-	localClient.updateClient(client);
-	client = new SubscribedClient();
-	bool = false;
-	clients = localClient.findAllClient();
-	return null ;
-}
+		localClient.deleteClient(client);
+		client = new SubscribedClient();
+		bool = false;
+		clients = localClient.findAllClient();
+		return null;
+	}
 
-public String hideAddAgentDialog(){
-	agent = new Agent();
-	
-	return "PF('agentAdd').hide();";
-}
+	public String doUpdateClient() {
 
+		localClient.updateClient(client);
+		client = new SubscribedClient();
+		bool = false;
+		clients = localClient.findAllClient();
+		return null;
+	}
 
-	
-	
+	public String hideAddAgentDialog() {
+		agent = new Agent();
 
+		return "PF('agentAdd').hide();";
+	}
 
 }
--- jsfproject/src/jsfproject/bean/AuthentificationBean.java
+++ jsfproject/src/jsfproject/bean/AuthentificationBean.java
@@ -6,9 +6,8 @@ import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-
-
 
 import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.Administrator;
 import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.Agent;
@@ -19,16 +18,17 @@ import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.Reporter;
 import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.SubscribedClient;
 import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.User;
 import tn.edu.esprit.pidev.artofdev.liveup.ejb.services.user.UserServicesLocal;
+
 @ManagedBean
 @SessionScoped
-public class AuthentificationBean implements Serializable{
+public class AuthentificationBean implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private String login;
-	private String password ;
+	private String password;
 	private boolean bool;
 	private boolean boolAdministrator;
 	private boolean boolReporter;
@@ -41,98 +41,119 @@ public class AuthentificationBean implements Serializable{
 	private UserServicesLocal userLocal;
 	private User user = new User();
 	private String userType;
-	
-	
-	
-	
-	
+
 	public boolean isBoolAdministrator() {
 		return boolAdministrator;
 	}
+
 	public void setBoolAdministrator(boolean boolAdministrator) {
 		this.boolAdministrator = boolAdministrator;
 	}
+
 	public boolean isBoolReporter() {
 		return boolReporter;
 	}
+
 	public void setBoolReporter(boolean boolReporter) {
 		this.boolReporter = boolReporter;
 	}
+
 	public boolean isBoolAgent() {
 		return boolAgent;
 	}
+
 	public void setBoolAgent(boolean boolAgent) {
 		this.boolAgent = boolAgent;
 	}
+
 	public boolean isBoolClient() {
 		return boolClient;
 	}
+
 	public void setBoolClient(boolean boolClient) {
 		this.boolClient = boolClient;
 	}
+
 	public boolean isBoolChefEditor() {
 		return boolChefEditor;
 	}
+
 	public void setBoolChefEditor(boolean boolChefEditor) {
 		this.boolChefEditor = boolChefEditor;
 	}
+
 	public boolean isBoolJournalist() {
 		return boolJournalist;
 	}
+
 	public void setBoolJournalist(boolean boolJournalist) {
 		this.boolJournalist = boolJournalist;
 	}
+
 	public boolean isBoolFreeLance() {
 		return boolFreeLance;
 	}
+
 	public void setBoolFreeLance(boolean boolFreeLance) {
 		this.boolFreeLance = boolFreeLance;
 	}
+
 	public String getUserType() {
 		return userType;
 	}
+
 	public void setUserType(String userType) {
 		this.userType = userType;
 	}
-	
+
 	public UserServicesLocal getUserLocal() {
 		return userLocal;
 	}
+
 	public void setUserLocal(UserServicesLocal userLocal) {
 		this.userLocal = userLocal;
 	}
+
 	public User getUser() {
 		return user;
 	}
+
 	public void setUser(User user) {
 		this.user = user;
 	}
+
 	public boolean isBool() {
 		return bool;
 	}
+
 	public void setBool(boolean bool) {
 		this.bool = bool;
 	}
+
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public AuthentificationBean(String login, String password) {
 		super();
 		this.login = login;
 		this.password = password;
 	}
-	
+
 	public AuthentificationBean() {
-		
+
 		boolAdministrator = false;
 		boolAgent = false;
 		boolChefEditor = false;
@@ -140,27 +161,31 @@ public class AuthentificationBean implements Serializable{
 		boolReporter = false;
 		boolFreeLance = false;
 		boolClient = false;
-		
+
 	}
+
 	public String doLogin() {
-//		bool = false;
-//		String msg = null;
-//		if (login.equals("seif")&&password.equals("seif")){
-//			bool = true;
-//			msg ="/pages/administrator/createAdmin?faces-redirect=true" ;
-//		}
-//		else {
-//			//FacesMessage message = new FacesMessage("please Verify your entries !");
-//			//FacesContext.getCurrentInstance().addMessage("form23:loginSubmit", message);
-//			
-//		}
-//		return msg;
-		
+		// bool = false;
+		// String msg = null;
+		// if (login.equals("seif")&&password.equals("seif")){
+		// bool = true;
+		// msg ="/pages/administrator/createAdmin?faces-redirect=true" ;
+		// }
+		// else {
+		// //FacesMessage message = new
+		// FacesMessage("please Verify your entries !");
+		// //FacesContext.getCurrentInstance().addMessage("form23:loginSubmit",
+		// message);
+		//
+		// }
+		// return msg;
+
 		boolAdministrator = false;
 		String nav = null;
+		//FacesMessage msg = null; 
 		user = userLocal.loginJSF(user.getLogin(), user.getPwd());
-		if(user != null){
-			bool =  true ;
+		if (user != null) {
+			bool = true;
 			if (user instanceof Administrator) {
 				userType = "Administrator";
 				boolAdministrator = true;
@@ -188,36 +213,38 @@ public class AuthentificationBean implements Serializable{
 			}
 			if (user instanceof FreeLance) {
 				userType = "FreeLance";
-				boolFreeLance = true ;
+				boolFreeLance = true;
 				nav = "/pages/FreeLance/freeLanceHome?faces-redirect=true";
 			}
 			if (user instanceof Agent) {
 				userType = "Agent";
-				boolAgent = true ;
+				boolAgent = true;
 				nav = "/pages/Agent/agentHome?faces-redirect=true";
 			}
+
+		} else {
+			//msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials");
+			//return "register?faces-redirect=true";
 			
+			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					"Erreur ","Invalid Credentials");
+
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+			return null;
 		}
-		else
-		{
-			
-		return "register?faces-redirect=true";
-		}
-		
-		return nav ;
-			
+
+		return nav;
+
 	}
-	
-	public String doLogout(){
-		bool =  false;
-//		login = null ; 
-//		password = null ;
-		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().clear();
+
+	public String doLogout() {
+		bool = false;
+		// login = null ;
+		// password = null ;
+		FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+				.clear();
 		return "/register?faces-redirect=true";
-		
-		
+
 	}
-	
-	
 
 }
